db1405d671ee969f1e796a6c48b4bd6e599c24d2
bomi0320 <84120820+bomi0320@users.noreply.github.com>
2025-06-30 19:36:22 +0900

    Add files via upload

---
+++ 05.memorydb/memorydb/src/main/java/com/example/memorydb/config/DataBaseConfig.java
@@ -0,0 +1,14 @@
+package com.example.memorydb.config;
+
+import com.example.memorydb.user.db.UserRepository;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+//public class DataBaseConfig {
+//
+//    public UserRepository userRepository(){
+//        return new UserRepository();
+//
+//    }
+//}
---
+++ 05.memorydb/memorydb/src/main/java/com/example/memorydb/db/DataRepository.java
@@ -0,0 +1,19 @@
+package com.example.memorydb.db;
+
+import java.util.Optional;
+import java.util.List;
+
+public interface DataRepository<T, ID> extends Repository<T, ID> {
+
+    // create, update
+    T save(T data);
+
+    // read
+    Optional<T> findById(ID id);
+
+    List<T> findAll();
+
+    // delete
+    void delete(ID id);
+}
+
